5ffdfbfd5491c2ddf3e255030e5a3285f22cd07f
bautistaralphangelo <ralph.bautista@seer-technologies.com>
2017-11-26 00:00:00 +0800

    Added update blog functionality Added delete blog functionality Expose mongodb to another port Modified swagger file

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ class JSONEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 
-client = MongoClient('mongodb://backend:27017/dockerdemo')
+client = MongoClient('mongodb://backend:3306/dockerdemo')
 db = client.blogpostDB
 
 app = Flask(__name__)
@@ -23,7 +23,7 @@ app = Flask(__name__)
 @app.route("/")
 def landing_page():
     posts = get_all_posts()
-    
+
     return render_template('blog.html', posts=json.loads(posts))
 
 
@@ -47,7 +47,7 @@ def remove_all():
 
 @app.route("/posts", methods=['GET'])
 def get_all_posts():
-    
+
     _posts = db.blogpostDB.find()
     posts = [post for post in _posts]
     return JSONEncoder().encode(posts)
@@ -69,9 +69,59 @@ def new():
 
 
 ### Insert function here ###
+@app.route('/post_get', methods=['GET'])
+def post_get():
+    post = get_post()
+
+    return render_template('edit_blog.html', post=json.loads(post))
+
+@app.route('/post_update', methods=['POST'])
+def post_update():
+    update_post()
+
+    return redirect(url_for('landing_page'))
 
 
+@app.route('/post_delete', methods=['GET'])
+def post_delete():
+    delete_post()
 
+    return redirect(url_for('landing_page'))
+
+
+@app.route('/post/get', methods=['GET'])
+def get_post():
+    post_id = request.args.get('id')
+    post = db.blogpostDB.find_one({'_id' : ObjectId(post_id)})
+
+    return JSONEncoder().encode(post)
+
+@app.route('/post/update', methods=['POST'])
+def update_post():
+    post_id = request.form['id']
+    post = {
+        'title': request.form['title'],
+        'post': request.form['post']
+    }
+
+    update_result = db.blogpostDB.update_one({'_id' : ObjectId(post_id)}, {'$set' : post})
+    response = {
+        'modified_count': update_result.matched_count
+    }
+
+    return JSONEncoder().encode(response)
+
+@app.route('/post/delete', methods=['GET'])
+def delete_post():
+    post_id = request.args['id']
+
+    delete_result = db.blogpostDB.delete_one({'_id' : ObjectId(post_id)})
+
+    response = {
+        'deleted_count': delete_result.deleted_count
+    }
+
+    return JSONEncoder().encode(response)
 ############################
 
 
